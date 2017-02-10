9eb62e29318aef6ac989232154c5e67a886733aa
daijianlin <daijianlin@163.com>
2017-02-10 12:37:33 +0800

    add , in before_action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user! only: [:create]
+  before_action :authenticate_user!, only: [:create]
   def show
     @order = Order.find(params[:id])
     @product_lists = @order.product_lists
