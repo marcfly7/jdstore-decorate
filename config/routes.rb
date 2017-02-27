3ea9e24a0a15a30283f352a3687e7f06d6deeed8
daijianlin <daijianlin@163.com>
2017-02-28 01:07:25 +0800

    add admin/orders

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'products#home'
   namespace :admin do
     resources :products
+    resources :orders
   end
   devise_for :users
   resources :products do
@@ -26,7 +27,7 @@ Rails.application.routes.draw do
   resources :orders do
     member do
       post :pay_with_alipay
-      post :pay_with_wechat 
+      post :pay_with_wechat
     end
   end
   namespace :account do
