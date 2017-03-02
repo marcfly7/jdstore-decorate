4885ea8aa82b3678235d8cee1da9e368ec023e83
daijianlin <daijianlin@163.com>
2017-03-02 19:12:00 +0800

    add part of navbar fixed when scrolling down

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,38 @@
 //= require bootstrap-sprockets
 //= require bootstrap/modal
 //= require_tree .
+//
+//
+// $(function(){
+//  var shrinkHeader = 120;
+//   $(window).scroll(function() {
+//     var scroll = getCurrentScroll();
+//       if ( scroll < shrinkHeader ) {
+//            $('.header-scroll').hide();
+//         }
+//         else {
+//             $('.header-scroll').show();
+//         }
+//   });
+// function getCurrentScroll() {
+//     return window.pageYOffset || document.documentElement.scrollTop;
+//     }
+// });
+
+
+
+$(window).scroll(function () {
+
+  if ($(this).scrollTop() > 200) {
+    if ($('.row2').is(':animated')) {
+      return false
+    }
+    $('.row2').addClass('header_fixed') // 让导航栏固定在顶部
+    $('.row2').stop().animate({top: 0}, 600) // 在600ms内，慢慢地出来
+    $('.header_placeholder').show() // 显示「用来占位」的header
+  } else {
+    $('.row2').css({top: -80})
+    $('.row2').removeClass('header_fixed')
+    $('.header_placeholder').hide()
+  }
+})
