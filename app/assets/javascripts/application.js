4fc15078c033bf1723d23119a54a34cfcdee21f3
daijianlin <daijianlin@163.com>
2017-03-02 16:37:53 +0800

    test2 to be deleted

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,20 @@
 //= require bootstrap-sprockets
 //= require bootstrap/modal
 //= require_tree .
+// 
+//
+// $(function(){
+//  var shrinkHeader = 300;
+//   $(window).scroll(function() {
+//     var scroll = getCurrentScroll();
+//       if ( scroll >= shrinkHeader ) {
+//            $('.header').addClass('shrink');
+//         }
+//         else {
+//             $('.header').removeClass('shrink');
+//         }
+//   });
+// function getCurrentScroll() {
+//     return window.pageYOffset || document.documentElement.scrollTop;
+//     }
+// });
