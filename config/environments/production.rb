67c9de07371eb1a8ddfc92130903229f58345887
daijianlin <daijianlin@163.com>
2017-03-06 16:11:34 +0800

    add url option in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,5 +82,7 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
+
+  config.action_mailer.default_url_options = { host: 'iknew.herokuapp.com' }
   config.active_record.dump_schema_after_migration = false
 end
